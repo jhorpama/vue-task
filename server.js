f8841424bb0b32ec7fb2e1d35a7e51b3b0ea1fdb
jhor pama <jkpama@misena.edu.co>
2019-06-19 08:36:46 -0500

    the server is added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const serverStatic = require('serve-static');
+const path = require('path');
+
+// Initialization
+const app = express();
+
+// Static Files
+app.use(serverStatic(path.join(__dirname, 'dist')));
+
+// Setting
+const port = process.env.PORT || 80;
+
+// Run server
+app.listen(port);
